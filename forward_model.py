Fix memory leakage in the pipeline

forward_model.py
@@ -325,8 +325,10 @@ def add_intrinsic_alignment(gamma_lensing, A_ia, eta_ia, overdensity_array, z_bi
     kappa_ia = intrinsic_alignments(overdensity_array, z_bin_edges, cosmo_params, A_ia, eta_ia)
 
     gamma_ia = kappa2shear(kappa_ia, lmax=lmax, verbose=verbose)
+    del kappa_ia
 
     gamma_lensing += gamma_ia
+    del gamma_ia
 
     return gamma_lensing
 
@@ -366,7 +368,8 @@ def add_shape_noise(shear_map, ra, dec, e1, e2, w):
     theta = (90-dec)*np.pi/180
     phi = ra*np.pi/180
     pix = hp.ang2pix(nside, theta, phi, nest=False)
-
+    del theta, phi
+    
     n_map = np.zeros(hp.nside2npix(nside))
 
     #Get a map between pixels in Healpix and galaxies in the galaxy catalog
@@ -379,6 +382,7 @@ def add_shape_noise(shear_map, ra, dec, e1, e2, w):
 
     #Apply random rotations to e1/2 to erase cosmological signal
     e1_rot, e2_rot = apply_random_rotation(e1, e2)
+    del e1, e2
 
     #noise maps
     noise_map_e1 = np.zeros(hp.nside2npix(nside))

run.py
@@ -2,6 +2,8 @@ import os
 import yaml
 import argparse
 import time
+import copy
+import gc
 
 import numpy as np
 import scipy.stats as stats
@@ -198,7 +200,7 @@ if __name__ == '__main__':
                 if verbose:
                     print(f"[!] Performing the forward model for the rotation of {j*360/5} degrees in RA and {rot_footprint_angle[k]} degrees in DEC...")
                 start_ = time.time()
-                output_ = output.copy()
+                output_ = copy.deepcopy(output)
                 nuisance_parameters = {}
                 gamma_lensing = np.load(path_output+f'/gamma_lensing_sim{sim_idx:05d}_nside{nside:04d}.npy')
                 if add_ia == 'T':
@@ -270,7 +272,6 @@ if __name__ == '__main__':
                         if verbose:
                             print("[!] Load the galaxy catalog...")
                         path_cat = config['shape_noise']['path_gal'] 
-                        cat_gal = fits.getdata(path_cat)
                         ra, dec, e1, e2, w = load_sources(path_cat, config['shape_noise'])
 
                         ra, dec, e1, e2 = get_rotation(ra, dec, e1, e2, j, k, verbose)
@@ -350,12 +351,16 @@ if __name__ == '__main__':
                             nuisance_parameters[f'bin_{i+1}'][f'eta'] = eta
                             del sys_map, idx_star
 
+                    del ra, dec, e1, e2, w
+
                         
 
                 output_['nuisance_parameters'] = nuisance_parameters
                 #Save the output
                 np.save(path_output+f'/forward_model_sim{sim_idx:05d}_nside{nside:04d}_rot{j}{k}_noisereal{noise_real}.npy', output_)
+                output_.clear()
                 del output_
+                gc.collect()
                 if verbose:
                     print(f"[!] The forward model for rotation {j}{k} is done.")
                     print(f"[!] The forward model for rotation {j}{k} took {(time.time()-start_)/60:.2f} minutes.")

utils.py
@@ -224,14 +224,15 @@ def rotate_gals(ras, decs, gammas1, gammas2, rotangle_rad=None, rotangle_dec=Non
         assert rot is not None, "rot or rotangle_rad/dec must be specified."
     # Map (ra, dec) --> (theta, phi)
     if units=="deg":
-        decs_rad = decs * np.pi/180.
-        ras_rad = ras * np.pi/180.
+        decs = decs * np.pi/180.
+        ras = ras * np.pi/180.
         if rotangle_rad is not None:
             rotangle_rad = rotangle_rad * np.pi/180.
         if rotangle_dec is not None:
             rotangle_dec = rotangle_dec * np.pi/180.
-    thetas = np.pi/2. + decs_rad
-    phis = ras_rad
+    thetas = np.pi/2. + decs
+    phis = ras
+    del decs, ras
     
     # Compute rotation angle
     if rot is None:
@@ -240,16 +241,19 @@ def rotate_gals(ras, decs, gammas1, gammas2, rotangle_rad=None, rotangle_dec=Non
         thisrot = rot
     rotatedthetas, rotatedphis = thisrot(thetas,phis, inv=False)
 
-    gamma_rot = (gammas1+1J*gammas2) * np.exp(1J * 2 * thisrot.angle_ref(rotatedthetas, rotatedphis,inv=True))
+    angle_ref = thisrot.angle_ref(rotatedthetas, rotatedphis, inv=True)
+    gamma_rot_1 = gammas1 * np.cos(2*angle_ref) - gammas2 * np.sin(2* angle_ref)
+    gamma_rot_2 = gammas1 * np.sin(2*angle_ref) + gammas2 * np.cos(2*angle_ref)
     
     # Transform back to (ra,dec)
     ra_rot = rotatedphis
     dec_rot = rotatedthetas - np.pi/2.
+    del rotatedphis, rotatedthetas
     if units=="deg":
         dec_rot *= 180./np.pi
         ra_rot *= 180./np.pi
     
-    return ra_rot, dec_rot, gamma_rot.real, gamma_rot.imag
+    return ra_rot, dec_rot, gamma_rot_1, gamma_rot_2
 
 def load_sources(path, config, cat_type='gal'):
     """
@@ -278,21 +282,23 @@ def load_sources(path, config, cat_type='gal'):
 
     data = Table.read(path)
     
-    rac = data[config['ra_col']]
-    dec = data[config['dec_col']]
+    rac = np.array(data[config['ra_col']])
+    dec = np.array(data[config['dec_col']])
     if cat_type == 'gal':
-        weight = data[config['w_col']]
-        e_1 = data[config['e1_col']]
-        e_2 = data[config['e2_col']]
+        weight = np.array(data[config['w_col']])
+        e_1 = np.array(data[config['e1_col']])
+        e_2 = np.array(data[config['e2_col']])
     elif cat_type == 'star':
-        e_1_star = data[config['e1_star_col']]
-        e_2_star = data[config['e2_star_col']]
-        e_1_psf = data[config['e1_psf_col']]
-        e_2_psf = data[config['e2_psf_col']]
-        size_star = data[config['size_star_col']]**2 if config['square_size'] else data[config['size_star_col']]
+        e_1_star = np.array(data[config['e1_star_col']])
+        e_2_star = np.array(data[config['e2_star_col']])
+        e_1_psf = np.array(data[config['e1_psf_col']])
+        e_2_psf = np.array(data[config['e2_psf_col']])
+        size_star = np.array(data[config['size_star_col']])**2 if config['square_size'] else np.array(data[config['size_star_col']])
         size_psf = data[config['size_psf_col']]**2 if config['square_size'] else data[config['size_psf_col']]
     else:
         raise ValueError("cat_type must be 'gal' or 'star'.")
+
+    del data
 
     masks_conditions = [
         lambda rac, dec: (rac < 50),
@@ -309,20 +315,10 @@ def load_sources(path, config, cat_type='gal'):
     for condition, rot_rad, rot_dec in zip(masks_conditions, rotangles_rad, rotangles_dec):
         gal_2_rot = condition(rac, dec)
         if cat_type == 'gal':
-            ra_rot,dec_rot,e_1_rot,e_2_rot = rotate_gals(ras=rac[gal_2_rot],decs=dec[gal_2_rot],gammas1=e_1[gal_2_rot], gammas2=e_2[gal_2_rot],rotangle_rad= rot_rad, rotangle_dec= rot_dec)
-            dec[gal_2_rot] = dec_rot
-            rac[gal_2_rot] = ra_rot
-            e_1[gal_2_rot] = e_1_rot
-            e_2[gal_2_rot] = e_2_rot
+            rac[gal_2_rot], dec[gal_2_rot], e_1[gal_2_rot], e_2[gal_2_rot] = rotate_gals(ras=rac[gal_2_rot],decs=dec[gal_2_rot],gammas1=e_1[gal_2_rot], gammas2=e_2[gal_2_rot],rotangle_rad= rot_rad, rotangle_dec= rot_dec)
         elif cat_type == 'star':
-            ra_rot, dec_rot, e_1_star_rot, e_2_star_rot = rotate_gals(ras=rac[gal_2_rot], decs=dec[gal_2_rot], gammas1=e_1_star[gal_2_rot], gammas2=e_2_star[gal_2_rot], rotangle_rad=rot_rad, rotangle_dec=rot_dec)
-            _, _, e_1_psf_rot, e_2_psf_rot = rotate_gals(ras=rac[gal_2_rot], decs=dec[gal_2_rot], gammas1=e_1_psf[gal_2_rot], gammas2=e_2_psf[gal_2_rot], rotangle_rad=rot_rad, rotangle_dec=rot_dec)
-            dec[gal_2_rot] = dec_rot
-            rac[gal_2_rot] = ra_rot
-            e_1_star[gal_2_rot] = e_1_star_rot
-            e_2_star[gal_2_rot] = e_2_star_rot
-            e_1_psf[gal_2_rot] = e_1_psf_rot
-            e_2_psf[gal_2_rot] = e_2_psf_rot
+            _, _, e_1_star[gal_2_rot], e_2_star[gal_2_rot] = rotate_gals(ras=rac[gal_2_rot], decs=dec[gal_2_rot], gammas1=e_1_star[gal_2_rot], gammas2=e_2_star[gal_2_rot], rotangle_rad=rot_rad, rotangle_dec=rot_dec)
+            rac[gal_2_rot], rac[gal_2_rot], e_1_psf[gal_2_rot], e_2_psf[gal_2_rot] = rotate_gals(ras=rac[gal_2_rot], decs=dec[gal_2_rot], gammas1=e_1_psf[gal_2_rot], gammas2=e_2_psf[gal_2_rot], rotangle_rad=rot_rad, rotangle_dec=rot_dec)
     
     if cat_type == 'gal':
         return rac,dec,e_1,e_2,weight